7b253d831428db11fc42d3240c26eb95028489d4
Infosec Gurung <72032538+InfosecGurung@users.noreply.github.com>
2021-07-12 16:49:26 +0530

    Renamed "Audacity" to "Audacium"
    Changes only made in comments.

--- src/AColorResources.h
+++ src/AColorResources.h
@@ -1,6 +1,6 @@
 /**********************************************************************
 
-  Audacity: A Digital Audio Editor
+  Audacium: A Digital Audio Editor
 
   @file AColorResources.h
   @brief RGB data of 'Color (New)' spectrogram color scheme
